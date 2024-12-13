e7ceeec4a88ae31a4f39ace63b1d73837fa7329d
Youness-Ailal <itsyounessaylal@gmail.com>
2024-12-13 16:50:04 +0100

    added remaining static pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
+import astroI18next from "astro-i18next";
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
-
-import astroI18next  from "astro-i18next"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ventrad.co",
 
-  integrations: [tailwind(), icon(), sitemap(),astroI18next()],
+  integrations: [tailwind(), icon(), sitemap(), astroI18next()],
   vite: {
     ssr: {
       noExternal: ["webcoreui"],
