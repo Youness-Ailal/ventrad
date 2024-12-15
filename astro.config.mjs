9b849e3cd7dc03de394a7f842e3b1d514e5849d9
Youness-Ailal <itsyounessaylal@gmail.com>
2024-12-15 12:15:58 +0100

    finished academy page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ventrad.co",
 
-  integrations: [tailwind(), icon(), sitemap(), astroI18next()],
+  integrations: [tailwind(), icon(), sitemap(), astroI18next(), mdx()],
   vite: {
     ssr: {
       noExternal: ["webcoreui"],
